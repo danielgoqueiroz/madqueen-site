902059b6be9ecf5ae5c0b3f440380823c30524e4
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-02-08 22:09:54 -0300

    Adicionado link de redes sociais e imagens de integrantes

--- components/global/Integrantes.vue
+++ components/global/Integrantes.vue
@@ -1,31 +1,70 @@
 <template>
-  <b-container>
+  <div class="integrants">
     <b-carousel
       id="carousel-1"
       v-model="slide"
       :interval="4000"
       controls
       indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
+      img-width="1080"
+      img-height="481"
       style="text-shadow: 1px 1px 2px #333"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
       <Integrante
-        name="Manu"
-        about="Sobre o integrante"
-        image="https://picsum.photos/600/300/?image=25"
+        :name="integrante.name"
+        :about="integrante.about"
+        :image="integrante.image"
+        v-for="integrante in integrantes"
+        :key="integrante.name"
       />
     </b-carousel>
-  </b-container>
+  </div>
 </template>
 
 <script>
 import Integrante from './Integrante.vue'
 export default {
+  data() {
+    return {
+      integrantes: [
+        {
+          name: 'Manu',
+          about: 'Vocal',
+          image: require('@/static/slide_manu.jpg'),
+        },
+        {
+          name: 'Dani',
+          about: 'Baixo',
+          image: require('@/static/slide_dani.jpg'),
+        },
+        {
+          name: 'Márcio',
+          about: 'Guitarra',
+          image: require('@/static/slide_marcio.jpg'),
+        },
+      ],
+
+      slide: 0,
+      sliding: null,
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    },
+  },
   components: { Integrante },
 }
 </script>
-<style></style>
+<style>
+.integrants {
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+</style>
--- components/global/SocialMedias.vue
+++ components/global/SocialMedias.vue
@@ -1,14 +1,41 @@
 <template>
-  <b-container>
-    <b-icon-file-earmark-font />
+  <b-container class="mx-auto social">
+    <b-button
+      :variant="color"
+      pill
+      href="https://www.instagram.com/madqueenrock/"
+      target="_blank"
+    >
+      <b-icon-instagram />
+    </b-button>
+    <b-button
+      :variant="color"
+      pill
+      href="https://www.facebook.com/madqueenrock/"
+      target="_blank"
+    >
+      <b-icon-facebook />
+    </b-button>
   </b-container>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      color: 'dark',
+    }
   },
 }
 </script>
-<style></style>
+<style>
+#b-button:hover {
+  background: rgb(0, 35, 89);
+}
+
+.social {
+  text-align: center;
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+</style>
